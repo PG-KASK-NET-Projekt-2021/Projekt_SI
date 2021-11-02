d9353d16c7d177310a3e23c937682ac57fd21b4a
marcel2012 <13705842+marcel2012@users.noreply.github.com>
2021-11-02 20:17:56 +0100

    gui app

---
+++ gui/SensorData.cs
@@ -0,0 +1,11 @@
+namespace gui
+{
+    public class SensorData
+    {
+        public int SensorId { get; set; }
+
+        public int SensorType { get; set; }
+
+        public int Value { get; set; }
+    }
+}
---
+++ gui/SensorQueueData.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace gui
+{
+    public class SensorQueueData : SensorData
+    {
+        public DateTime Date { get; set; }
+    }
+}
