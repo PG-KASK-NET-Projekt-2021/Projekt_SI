eb3fa2fbd8c3d5309c37589557e65fa90132b502
Etav99 <etavpl@gmail.com>
2021-12-08 12:48:21 +0100

    Add conversion

--- SI_API/DataConverter.cs
+++ SI_API/DataConverter.cs
@@ -12,20 +12,29 @@ namespace SI_API
             sensorDataModel.Date = sensorData.Date;
             sensorDataModel.SensorId = sensorData.SensorId;
             sensorDataModel.SensorType = sensorData.SensorType;
-            sensorDataModel.Value = sensorData.Value;
             sensorDataModel._id = ObjectId.Empty;
+            sensorDataModel.Value = ConvertValue((Sensor)sensorData.SensorId, sensorData.Value);
 
             return sensorDataModel;
         }
+
+        private static float ConvertValue(Sensor sensorId, int value)
+        {
+            // Tuple<int, int> interval = MapInterval(sensorId);
+            // int start = interval.Item1;
+            // int end = interval.Item2;
+            return (float)value / 100;
+        }
+        
         
         private static Tuple<int, int> MapInterval(Sensor sensorId)
         {
             return sensorId switch
             {
-                Sensor.Termometr => new Tuple<int, int>(-40, 50),
-                Sensor.Barometr => new Tuple<int, int>(960, 1060),
-                Sensor.Higrometr => new Tuple<int, int>(0, 100),
-                Sensor.Fotometr => new Tuple<int, int>(20, 110000),
+                Sensor.Termometr => new Tuple<int, int>(-4000, 5000),
+                Sensor.Barometr => new Tuple<int, int>(96000, 106000),
+                Sensor.Higrometr => new Tuple<int, int>(0, 10000),
+                Sensor.Fotometr => new Tuple<int, int>(2000, 11000000),
                 _ => null
             };
         }
